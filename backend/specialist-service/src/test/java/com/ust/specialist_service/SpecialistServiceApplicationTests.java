89f4bde18668339cd4a5a76fb19a540ecfe20b95
Abijith S <abijithsuresh47@gmail.com>
2024-11-02 17:13:54 +0530

    Started specialist service

---
+++ backend/specialist-service/src/test/java/com/ust/specialist_service/SpecialistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.specialist_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
