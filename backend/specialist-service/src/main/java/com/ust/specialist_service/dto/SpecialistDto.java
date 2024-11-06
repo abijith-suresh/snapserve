390e7a3595fb49816fcca15a8472966163c4a023
Aarzoo0 <aarjoochougule.scoe.it@gmail.com>
2024-11-06 10:23:47 +0530

    updated specialist-service with dto

---
+++ backend/specialist-service/src/main/java/com/ust/specialist_service/dto/SpecialistDto.java
@@ -0,0 +1,10 @@
+package com.ust.specialist_service.dto;
+
+import lombok.Data;
+
+@Data
+public class SpecialistDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
--- backend/specialist-service/src/main/java/com/ust/specialist_service/service/SpecialistService.java
+++ backend/specialist-service/src/main/java/com/ust/specialist_service/service/SpecialistService.java
@@ -1,5 +1,6 @@
 package com.ust.specialist_service.service;
 
+import com.ust.specialist_service.dto.SpecialistDto;
 import com.ust.specialist_service.entity.Specialist;
 import com.ust.specialist_service.repo.SpecialistRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,21 +8,50 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SpecialistService {
     @Autowired
     private SpecialistRepo specialistRepo;
 
-    public Specialist createSpecialist(Specialist specialist) {
-        return specialistRepo.save(specialist);
+    private void dtoToModel(Specialist specialist, SpecialistDto specialistDto) {
+       specialist.setFirstName(specialistDto.getFirstName());
+       specialist.setLastName(specialistDto.getLastName());
+        specialist.setEmail(specialistDto.getEmail());
+
     }
 
-    public List<Specialist> getAllSpecialists() {
-        return specialistRepo.findAll();
+    private SpecialistDto modelToDto(Specialist specialist) {
+       SpecialistDto specialistDto= new SpecialistDto();
+        specialistDto.setFirstName(specialist.getFirstName());
+        specialistDto.setLastName(specialist.getLastName());
+        specialistDto.setEmail(specialist.getEmail());
+
+        return specialistDto;
+    }
+
+
+
+    public Specialist createSpecialist(SpecialistDto specialistDto) {
+        Specialist specialist = new Specialist();
+        dtoToModel(specialist, specialistDto);
+        return specialistRepo.save(specialist);
+
+
+    }
+
+    public List<SpecialistDto> getAllSpecialists() {
+        List<Specialist> specialist = specialistRepo.findAll();
+
+        return specialist.stream()
+                .map(this::modelToDto)
+                .collect(Collectors.toList());
+
     }
 
     public Optional<Specialist> getSpecialistById(String id) {
+
         return specialistRepo.findById(id);
     }
 
@@ -31,6 +61,7 @@ public class SpecialistService {
     }
 
     public void deleteSpecialist(String id) {
+
         specialistRepo.deleteById(id);
     }
 }
