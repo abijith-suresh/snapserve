a42cdf3d79630833f8870abd5b8d5803c2284723
Abijith S <abijithsuresh47@gmail.com>
2024-11-06 09:26:55 +0530

    Updated naming of methods

--- backend/message-service/src/main/java/com/ust/message_service/controller/MessageController.java
+++ backend/message-service/src/main/java/com/ust/message_service/controller/MessageController.java
@@ -9,30 +9,30 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/messages")
+@RequestMapping("/api/messages")
 public class MessageController {
     @Autowired
     private MessageService messageService;
 
     @GetMapping
-    public List<Message> getAllCustomers(){
+    public List<Message> getAllMessages(){
 
         return messageService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Message> getCustomerById(@PathVariable String id){
+    public ResponseEntity<Message> getMessageById(@PathVariable String id){
         Message message= messageService.findById(id);
         return message != null ? ResponseEntity.ok(message) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public Message saveCustomer(@RequestBody Message message){
+    public Message saveMessage(@RequestBody Message message){
         return messageService.save(message);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCustomerById(@PathVariable String id){
+    public ResponseEntity<Void> deleteMessageById(@PathVariable String id){
         messageService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
--- backend/review-service/src/main/java/com/ust/review_service/controller/ReviewController.java
+++ backend/review-service/src/main/java/com/ust/review_service/controller/ReviewController.java
@@ -9,29 +9,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reviews")
+@RequestMapping("/api/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
     @GetMapping
-    public List<Review> getAllCustomers(){
+    public List<Review> getAllReviews(){
         return reviewService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Review> getCustomerById(@PathVariable String id){
+    public ResponseEntity<Review> getReviewById(@PathVariable String id){
        Review review= reviewService.findById(id);
         return review != null ? ResponseEntity.ok(review) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public Review saveCustomer(@RequestBody Review review){
+    public Review saveReview(@RequestBody Review review){
         return reviewService.save(review);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCustomerById(@PathVariable String id){
+    public ResponseEntity<Void> deleteReviewById(@PathVariable String id){
         reviewService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
