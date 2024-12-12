c74a29c4e1b3c5ec66e025a8725e3e4c0316e8da
Aarzoo0 <287982@ust.com>
2024-12-12 16:11:59 +0530

    admin updated

--- frontend/src/admin/LogOut.jsx
+++ frontend/src/admin/LogOut.jsx
@@ -33,20 +33,7 @@ const LogOut = () => {
           disabled={isLoggingOut}
           className="flex items-center justify-center gap-3 rounded-lg bg-white px-6 py-3 text-black font-semibold w-full transition-all duration-300 ease-in-out hover:bg-gray-400 disabled:bg-gray-600"
         >
-          {/* Spinner while logging out */}
-          <svg
-            className={`h-5 w-5 ${isLoggingOut ? 'animate-spin' : ''} transition-transform duration-500`}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
+          
           <span>{isLoggingOut ? 'Logging out...' : 'Log Out'}</span>
         </button>
 
