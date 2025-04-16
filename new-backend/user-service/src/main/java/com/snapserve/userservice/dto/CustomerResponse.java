25e69df153a495f924a43fe412bb7a7a6a94e4eb
Abijith S <abijithsuresh47@gmail.com>
2025-04-17 02:40:53 +0530

    Adding Customer, Specialist and Admin request and response dtos

---
+++ new-backend/user-service/src/main/java/com/snapserve/userservice/dto/CustomerResponse.java
@@ -0,0 +1,23 @@
+package com.snapserve.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerResponse {
+    private String id;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private byte[] profilePic;
+    private LocalDate dob;
+    private String gender;
+}
---
+++ new-backend/user-service/src/main/java/com/snapserve/userservice/dto/SpecialistRequest.java
@@ -0,0 +1,32 @@
+package com.snapserve.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SpecialistRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private byte[] profilePic;
+    private LocalDate dob;
+    private String gender;
+
+    private String jobTitle;
+    private String bio;
+    private Integer yearsOfExperience;
+    private List<String> certifications;
+    private List<String> servicesOffered;
+    private Double hourlyRate;
+    private Boolean isAvailable;
+}
