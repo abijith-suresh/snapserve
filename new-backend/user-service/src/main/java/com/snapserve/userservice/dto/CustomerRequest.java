301a6f136fdc687708f7fb0ab7b6b775b5482332
Abijith S <abijithsuresh47@gmail.com>
2025-04-18 13:19:30 +0530

    Adding validation to request dtos

--- new-backend/user-service/src/main/java/com/snapserve/userservice/dto/CustomerRequest.java
+++ new-backend/user-service/src/main/java/com/snapserve/userservice/dto/CustomerRequest.java
@@ -1,5 +1,7 @@
 package com.snapserve.userservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +14,27 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerRequest {
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String phoneNumber;
+
+    @NotBlank
     private String address;
+
     private byte[] profilePic;
+
+    @NotBlank
     private LocalDate dob;
+
+    @NotBlank
     private String gender;
 }
--- new-backend/user-service/src/main/java/com/snapserve/userservice/dto/SpecialistRequest.java
+++ new-backend/user-service/src/main/java/com/snapserve/userservice/dto/SpecialistRequest.java
@@ -1,5 +1,9 @@
 package com.snapserve.userservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,20 +17,45 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SpecialistRequest {
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String phoneNumber;
+
+    @NotBlank
     private String address;
+
     private byte[] profilePic;
+
+    @NotBlank
     private LocalDate dob;
+
+    @NotBlank
     private String gender;
 
+    @NotBlank
     private String jobTitle;
+
     private String bio;
+
+    @Positive
     private Integer yearsOfExperience;
-    private List<String> certifications;
-    private List<String> servicesOffered;
+
+
+    private List<@NotBlank String> certifications;
+    private List<@NotBlank String> servicesOffered;
+
+    @Positive
     private Double hourlyRate;
+
+    @NotNull
     private Boolean isAvailable;
 }
