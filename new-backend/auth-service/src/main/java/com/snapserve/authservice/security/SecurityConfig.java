da6635fe8377d4739f85a83c178e397e4677bfc5
Abijith S <abijithsuresh47@gmail.com>
2025-04-22 23:46:28 +0530

    Updating security config to remove authfilter

--- new-backend/auth-service/src/main/java/com/snapserve/authservice/security/SecurityConfig.java
+++ new-backend/auth-service/src/main/java/com/snapserve/authservice/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.snapserve.authservice.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,13 +17,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole(RoleConstants.ROLE_ADMIN)
@@ -30,13 +30,17 @@ public class SecurityConfig {
                         .hasAnyRole(RoleConstants.ROLE_CUSTOMER, RoleConstants.ROLE_ADMIN)
                         .requestMatchers("/specialist/**")
                         .hasAnyRole(RoleConstants.ROLE_SPECIALIST, RoleConstants.ROLE_ADMIN)
-                        .anyRequest().authenticated()
+                        .anyRequest()
+                        .authenticated()
                 )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
 
