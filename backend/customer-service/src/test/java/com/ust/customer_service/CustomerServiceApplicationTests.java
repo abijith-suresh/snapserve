406f2c9528609ee42f8e614f299b607a852571e9
Abijith <58354654+abijith-suresh@users.noreply.github.com>
2025-07-05 14:11:42 +0530

    fix: Disable discovery clients for all microservice test

--- backend/customer-service/src/test/java/com/ust/customer_service/CustomerServiceApplicationTests.java
+++ backend/customer-service/src/test/java/com/ust/customer_service/CustomerServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ust.customer_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false", "spring.cloud.discovery.enabled=false"})
 class CustomerServiceApplicationTests {
 
   @Test
